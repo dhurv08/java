213286be14f765f782674f3694db2796d9777ebf
dhruv08 <devander.kumra216@gmail.com>
2024-03-14 16:57:09 +0530

    java program

---
+++ Programs/dhruv.java
@@ -0,0 +1,10 @@
+//create a class with your name and in main function print about five line about you 
+class dhruv{
+    public  static void main(String args[]){
+    System.out.println("hi my name is dk");
+    System.out.println("i am 20 years old");
+    System.out.println("studying in daviet");
+    System.out.println("i live in jalandhar");
+    System.out.println("my son name is Mohit chauhan");
+    }
+}
