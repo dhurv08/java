27541a8c4f99f6f253e459e7f854cf29a79234df
dhruv08 <devander.kumra216@gmail.com>
2024-03-26 17:43:40 +0530

    updated question in the program

--- Programs/calciRunner.java
+++ Programs/calciRunner.java
@@ -1,3 +1,7 @@
+/*create a class named calci which is having two function sum and sub just printing welcome to sum / sub make sum function as static 
+create another class named calci runner , which is having main function to run the program , 
+code in such a way that you call both function from calci class and make sure you dont call sum with object .dd
+ */
 public class calciRunner{
     public static void main(String args[]){
         calci.sum();
