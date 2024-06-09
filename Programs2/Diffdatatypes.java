163a06de649b2fe2dfd8e87398c694c79957c26c
Devander Kumra <133108241+dhurv08@users.noreply.github.com>
2024-06-09 14:12:26 +0530

    Create a Java program to declare , initialize and print variables if all data types

---
+++ Programs2/Diffdatatypes.java
@@ -0,0 +1,16 @@
+package Programs2;
+
+public class Diffdatatypes {
+    public static void main(String[] args) {
+        int a = 15 ;
+        float b = (float) 3.14;
+        double c = 4.524532 ; 
+        char d = 'l';
+
+        System.out.println("Value of int a = " + a);
+        System.out.println("Value of float b = " + b);
+        System.out.println("Value of double c = " + c);
+        System.out.println("Value of char d = " + d );
+
+    }
+}
