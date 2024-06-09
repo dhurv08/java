66233d700e65fb2b0cf9327b99d9fbee0a554495
Devander Kumra <133108241+dhurv08@users.noreply.github.com>
2024-06-09 13:59:21 +0530

    java basic output program

---
+++ Programs2/basic.java
@@ -0,0 +1,15 @@
+package Programs2;
+/*Create a java program to print the following output
+
+**************************
+Name : 
+Course:
+College:
+Email:
+Qualification:
+*************************** */
+public class basic {
+    public static void main(String[] args) {
+        System.out.println("Name : Dhruv Kumra \nCourse : B.Tech \nCollege : DAVIET \nEmail : dkumra4@gmail.com \nQualification : 12th pass");
+    }
+}
