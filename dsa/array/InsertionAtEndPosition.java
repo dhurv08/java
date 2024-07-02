9f6dc1563d45aab79d44087116b0efed0b53e108
Devander Kumra <133108241+dhurv08@users.noreply.github.com>
2024-07-02 19:38:25 +0530

    Insertion in array at end position

---
+++ dsa/array/InsertionAtEndPosition.java
@@ -0,0 +1,38 @@
+package dsa.array;
+
+import java.util.Scanner;
+
+public class InsertionAtEndPosition {
+    public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+
+        int [] a = new int[10];
+        System.out.print("Enter how many elements you want to enter in ARRAY = ");
+        int n = scanner.nextInt();
+    
+        //Input ARRAY
+        System.out.println("Enter "+ n +" numbers in the Array : ");
+        for(int i = 0; i<n ;i++){
+            a[i] = scanner.nextInt();
+        }
+
+        System.out.print("Enter the new element = ");
+        int element = scanner.nextInt();
+        System.out.println("Inserting the new element at END position");
+
+        //Insertion Part       
+        if(n == 0){
+            System.out.println("The Array is full and we need to implement the New Pointer Array Method");
+        }else{
+            a[n] = element;
+            n = n+1;
+        }
+
+        
+        System.out.print("The new Array is : ");
+        for(int i = 0 ; i<n ; i++){
+            System.out.print(a[i] + "\t");
+        }
+        scanner.close();
+    }
+}
